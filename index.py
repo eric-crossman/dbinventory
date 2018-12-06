886005f73d9e7765d8d8ea354adf83257f107c43
Eric Crossman <eric_crossman@cox.net>
2018-12-06 10:19:27 -0500

    Correcting port assignment to retrieve from docker environment.

--- index.py
+++ index.py
@@ -43,5 +43,4 @@ def static_proxy(path):
 if __name__ == '__main__':
     LOG.info('running environment: %s', os.environ.get('ENV'))
     app.config['DEBUG'] = os.environ.get('ENV') == 'development' # Debug mode if development env
-    app.run(host='0.0.0.0', port=int(PORT)) # Run the app
-    
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT'))) # Run the app
